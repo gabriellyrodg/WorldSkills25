f0772a169ede4aa5815b013d96f04c6583059b3c
Gabzftonez Ezmknrz <you@e>
2025-05-19 20:11:47 -0300

    Adiciona ProgramNomeIdade.cs na pasta PrimeiraFolhaDeAtividades

---
+++ PrimeiraFolhaDeAtividades/ProgramNomeIdade.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static string NomeEIdade(string nome, string idade)
+    {
+        return $"Nome: {nome}\n Idade: {idade}";
+    }
+    static int Verificador(int verificacao)
+    {
+        if (verificacao >= 18) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+     static void Main()
+        {
+            Console.Write("Digite seu nome: ");
+            string NomePessoa = Console.ReadLine();
+            Console.WriteLine("Digite sua idade: ");
+            string Idadepessoa = Console.ReadLine();
+            Console.WriteLine(NomeEIdade(NomePessoa, Idadepessoa));
+            int idadeint = int.Parse(Idadepessoa);
+            int resultado = Verificador(idadeint);
+            if (resultado == 1)
+            {
+                Console.WriteLine("Voce é maior de idade!");
+            }
+            else
+            {
+                Console.WriteLine("Voce é menor de idade.");
+            }
+        }
+    }
+
