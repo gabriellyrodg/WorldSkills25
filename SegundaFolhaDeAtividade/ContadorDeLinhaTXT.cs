e21deb77a1f2dd9a1eaa7110ec93703728cbd257
Gabzftonez Ezmknrz <you@e>
2025-05-19 20:49:18 -0300

    Adiciona ContadorDeLinhaTXT.cs na SegundaFolhaDeAtividade

---
+++ SegundaFolhaDeAtividade/ContadorDeLinhaTXT.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ContadorDeLinhaTXT
+{
+    class Program
+    {
+        static void Main()
+        {
+            string arquivo = "contadordelinha.txt";
+            List<string> linhas = new List<string>();
+
+            Console.Write("Digite várias linhas em seu arquivo .txt.\n (Digite 'fim' para encerrar): ");
+
+            string entrada;
+            while ((entrada = Console.ReadLine()) != "fim")
+            {
+                linhas.Add(entrada);
+            }
+
+            File.WriteAllLines(arquivo, linhas);
+            Console.WriteLine($"\nO arquivo possui {linhas.Count} linha(s).");
+        }
+    }
+}
