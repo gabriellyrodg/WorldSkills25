b69b7320f56ac8b1c086a87c431e0c58329f68da
Gabzftonez Ezmknrz <you@e>
2025-05-19 20:12:44 -0300

    Adiciona ProgramListaNomesA.cs na pasta PrimeiraFolhaDeAtividades

---
+++ PrimeiraFolhaDeAtividades/ProgramListaNomesA.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] nomes = new string[5];
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"Digite o nome {i}:");
+            nomes[i] = Console.ReadLine();
+        }
+        Console.WriteLine("Sua lista de nomes é:" + string.Join(", ", nomes));
+        Console.Write("Agora digite a Inicial dos nomes que voce quer exibir:");
+        char inicial = Console.ReadLine()[0];
+        foreach(string nome in nomes)
+        {
+            if (nome.StartsWith(inicial))
+            {
+                Console.WriteLine($"O nome que começa com {inicial} na sua lista é {nome}");
+            }
+        }
+
+    }
+}
