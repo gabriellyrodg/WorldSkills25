6ef068aea99e71241d5a707ec1a73538fde3a780
Gabzftonez Ezmknrz <you@e>
2025-05-18 21:38:54 -0300

    Adicionando ArquivoDeAlunos.cs ao repositório

---
+++ ArquivoDeAlunos.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+
+class AlunoTXT
+{
+    static void Main()
+    {
+        string arquivo = "aluno.txt";
+        Console.Clear();
+        Console.WriteLine("Sistema de Alunos .txt\n Pressione qualquer tecla para entrar...");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.Write($"Digite o nome do Aluno {i + 1}: ");
+            string nome = Console.ReadLine();
+            Console.Write($"Digite a idade do Aluno {i + 1}: ");
+            int idade = int.Parse(Console.ReadLine());
+            Console.Write($"Qual o Curso do Aluno {i + 1}? ");
+            string curso = Console.ReadLine();
+            Console.Clear();
+            File.AppendAllText(arquivo, $"\n Nome: {nome}  |  Idade: {idade}  | Curso: {curso}  ");
+            Console.WriteLine("Adicionado com sucesso!");
+            Console.WriteLine("Deseja continuar? 1 - Sim  |  2 - Não\n Resposta: ");
+            int stop = int.Parse(Console.ReadLine());
+            if (stop == 1)
+            {
+                i = i;
+
+            }
+            else
+            {
+                Console.WriteLine("Até Mais!");
+                break;
+            }
+
+
+
+
+        }
+    }
+}
+
+
