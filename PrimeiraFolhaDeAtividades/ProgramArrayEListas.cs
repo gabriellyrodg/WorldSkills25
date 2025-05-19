9ec5e72643bd6cba30e4ca0ea436f770ee22961b
Gabzftonez Ezmknrz <you@e>
2025-05-19 20:09:57 -0300

    Adiciona ProgramArrayEListas.cs na pasta PrimeiraFolhaDeAtividades

---
+++ PrimeiraFolhaDeAtividades/ProgramArrayEListas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int soma = 0;
+        int[] number = { 4, 7, 9, 3, 1 };
+
+        foreach (int num in number)
+        {
+            soma += num; 
+        }
+
+        Console.WriteLine($"A soma total dos elementos é: {soma}");
+        double media = soma / 5;
+        Console.WriteLine($"A média é: {media}");
+    }
+}
