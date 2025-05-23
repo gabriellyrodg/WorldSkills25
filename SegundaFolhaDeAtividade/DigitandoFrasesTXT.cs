94962176d7424602fe53a8444b07dcfd8a58e077
Gabzftonez Ezmknrz <you@e>
2025-05-23 17:59:27 -0300

    Adiciona DigitandoFrasesTXT.cs na SegundaFolhaDeAtividade

---
+++ SegundaFolhaDeAtividade/DigitandoFrasesTXT.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        int contador = 1;
+
+        while (true)
+        {
+            Console.Write("Digite uma frase (ou 'sair' para encerrar): ");
+            string frase = Console.ReadLine();
+
+            if (frase.ToLower() == "sair")
+                break;
+
+            string nomeArquivo = $"frase{contador}.txt";
+
+            File.WriteAllText(nomeArquivo, frase);
+            Console.WriteLine($"Frase salva em '{nomeArquivo}'");
+
+            contador++;
+        }
+    }
+}
