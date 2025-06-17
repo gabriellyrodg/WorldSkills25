6f1837b052099ccebd6d66be7285cea56e210269
Gabzftonez Ezmknrz <you@e>
2025-06-16 21:10:33 -0300

    Salvar mudanças antes do pull

---
+++ SomaDeZeroaCem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int resultado = 0;
+
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.WriteLine(resultado);
+            resultado += i;
+        }
+    }
+}
